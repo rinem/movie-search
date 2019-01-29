149ae79bcbfadcfb5c7dd48ca927ff4d51aa9f81
Roshan Mishra <irinem139@gmail.com>
2019-01-29 15:16:55 +0530

    Add stylesheets

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var request = require("request");
 var port = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
